cc74b5fac9aa5e3ea9a17e7a2ccabbd161526b0d
yasmin0026 <rhina.espinoza201099@liveusam.edu.sv>
2021-05-29 08:42:21 -0600

    MVC
    MVC

---
+++ RegistroEventosUSAM/RegistroEventosUSAM/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RegistroEventosUSAM.MvcApplication" Language="C#" %>
---
+++ RegistroEventosUSAM/RegistroEventosUSAM/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RegistroEventosUSAM.Startup))]
+namespace RegistroEventosUSAM
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
