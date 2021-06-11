f05a9adc543f58de49fc8b5a0b45f941a9e16d84
yasmin0026 <rhina.espinoza201099@liveusam.edu.sv>
2021-06-10 23:34:54 -0600

    Reservación de eventos funcional
    Reservación de eventos funcional.

---
+++ ReservacionEventosUSAM/ReservacionEventosUSAM/Controllers/EventosController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReservacionEventosUSAM.Controllers
+{
+    public class EventosController : Controller
+    {
+        // GET: Eventos
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Feed()
+        {
+            return View();
+        }
+    }
+}
---
+++ ReservacionEventosUSAM/ReservacionEventosUSAM/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReservacionEventosUSAM.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Panel()
+        {
+            return View();
+        }
+
+        public ActionResult Salir()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
